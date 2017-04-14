104eae375fa86e304f8354fa27592524a952399c
BillDunlap <williamwdunlap@gmail.com>
2017-04-14 10:05:55 -0700

    Add @Override flags, add CircularBuffer<Integer> example, explicitly declare methods and fields public or private.

--- CircularBuffer.java
+++ CircularBuffer.java
@@ -4,37 +4,45 @@ import java.lang.UnsupportedOperationException;
 
 public class CircularBuffer<Type> implements Iterable<Type>
 {
-    Type[] values;
-    int nextInsertionPosition;
+    private Type[] values;
+    private int nextInsertionPosition;
 
     public CircularBuffer(Type[] values)
     {
         this.values = values;
         nextInsertionPosition = 0;
     }
-    void add(Type value)
+    public void add(Type value)
     {
         values[nextInsertionPosition] = value;
         nextInsertionPosition = (nextInsertionPosition + 1) % values.length ;
     }
     class CircularBufferIterator implements java.util.Iterator<Type>
     {
-        int current = 0;
+        private int current;
+        public CircularBufferIterator()
+        {
+            current = 0;
+        }
+        @Override
         public boolean hasNext()
         {
             return current < CircularBuffer.this.values.length;
         }
+        @Override
         public Type next()
         {
             Type retval = CircularBuffer.this.values[(current + CircularBuffer.this.nextInsertionPosition) % CircularBuffer.this.values.length];
             current = current + 1;
             return retval;
         }
+        @Override
         public void remove()
         {
             throw new UnsupportedOperationException();
         }
     }
+    @Override
     public Iterator<Type> iterator()
     {
         return new CircularBufferIterator();
@@ -44,11 +52,18 @@ public class CircularBuffer<Type> implements Iterable<Type>
     {
         String[] init = { "A", "B", "C", "D" } ;
         CircularBuffer<String> cb = new CircularBuffer<String>(init);
+        CircularBuffer<Integer> cb2 = new CircularBuffer<Integer>(new Integer[3]);
         cb.add("E");
+	    cb2.add(101);
         cb.add("F");
+	    cb2.add(102);
         for(String s : cb) // uses hasNext() and next() methods of CircularBuffer's iterator
         {
             System.out.println("    " + s);
         }
+	    for(Integer i : cb2)
+	    {
+		    System.out.println(" " + i);
+	    }
     }
 }
